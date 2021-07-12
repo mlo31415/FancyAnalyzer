88bb14ec41e6341f6335c542f0f05bfede592993
mlo31415 <mlo@baskerville.org>
2021-07-12 14:33:40 -0400

    Move most of the Local-specific code to a separate file

--- FancyAnalyzer.py
+++ FancyAnalyzer.py
@@ -6,9 +6,10 @@ import os
 import re
 from datetime import datetime
 
+from Locale import Locale
 from F3Page import F3Page, DigestPage, TagSet
 from Log import Log, LogOpen, LogSetHeader
-from HelpersPackage import WindowsFilenameToWikiPagename, SplitOnSpan, WikiExtractLink, CrosscheckListElement, WikiLinkSplit
+from HelpersPackage import WindowsFilenameToWikiPagename, SplitOnSpan, WikiExtractLink, CrosscheckListElement
 from ConInstanceInfo import ConInstanceInfo
 from FanzineIssueSpecPackage import FanzineDateRange
 
@@ -84,210 +85,8 @@ Log("   "+str(len(fancyPagesDictByWikiname))+" semi-unique links found")
 
 # Build a locale database
 Log("\n\n***Building a locale dictionary")
-locales: Set[str]=set()  # We use a set to eliminate duplicates and to speed checks
 for page in fancyPagesDictByWikiname.values():
-    if page.IsLocale:
-        LogSetHeader("Processing Locale "+page.Name)
-        locales.add(page.Name)
-    else:
-        if page.Redirect != "" and page.Redirect in fancyPagesDictByWikiname.keys():
-            if fancyPagesDictByWikiname[page.Redirect].IsLocale:
-                LogSetHeader("Processing Locale "+page.Name)
-                locales.add(page.Name)
-
-
-# Convert names like "Chicago" to "Chicago, IL"
-# We look through the locales database for names that are proper extensions of the input name
-# First create the dictionary we'll need
-localeBaseForms: Dict[str, str]={}  # It's defined as a dictionary with the value being the base form of the key
-for locale in locales:
-    # Look for names of the form Name,ST
-    m=re.match("^([A-Za-z .]*),\s([A-Z]{2})$", locale)
-    if m is not None:
-        city=m.groups()[0]
-        state=m.groups()[1]
-        localeBaseForms.setdefault(city, city+", "+state)
-
-# Find the base form of a locale.  E.g., the base form of "Cambridge, MA" is "Boston, MA".
-def BaseFormOfLocaleName(localeBaseForms: Dict[str, str], name: str) -> str:
-    # Handle the (few) special cases where names may be confusing.
-    # There are certain names which are the names of minor cities and towns (usually written as "Name, XX") and also important cities which are written just "Name"
-    # E.g., "London, ON" and "London" or "Dublin, OH" and "Dublin"
-    # When the name appears without state (or whatever -- this is mostly a US & Canada problem) if it's in the list below, we assume it's a base form
-    # Note that we only add to this list when there is a *fannish* conflict.
-    basetable=["London", "Dublin"]
-    if name in basetable:
-       return name
-
-    # OK, try to find a base name
-    if name in localeBaseForms.keys():
-        return localeBaseForms[name]
-    return name
-
-# The current algorithm messes up multi-word city names and only catches the last word.
-# Correct the ones we know of to the full name.
-multiWordCities={
-    "Angeles, CA": "Los",
-    "Antonio, TX": "San",
-    "Barbara, CA": "Santa",
-    "Beach, CA": ["Long", "Huntington"],
-    "Beach, FL": ["West Palm", "Cocoa", "Palm"],
-    "Beach, VA": "Virginia",
-    "Bend, IN": "South",
-    "Brook, IL": "Oak",
-    "Brook, LI": "Stony",
-    "Brook, NJ": "Saddle",
-    "Brook, NY": ["Stony", "Rye"],
-    "Brunswick, NJ": "New",
-    "Carrollton, MD": "New",
-    "Charles, IL": "St.",
-    "Christi, TX": "Corpus",
-    "City, IA": "Iowa",
-    "City, KY": "Park",
-    "City, MO": "Kansas",
-    "City, OK": "Oklahoma",
-    "City, UT": "Salt Lake",
-    "City, VA": "Crystal",
-    "Collins, CO": "Fort",
-    "Creek, CA": "Walnut",
-    "Creek, MI": "Battle",
-    "Diego, CA": "San",
-    "Elum, WA": "Cle",
-    "Falls, NY": "Niagara",
-    "Francisco, CA": "San",
-    "Grande, AZ": "Casa",
-    "Green, KY": "Bowling",
-    "Guardia, NY": "La",
-    "Harbor, NH": "Center",
-    "Heights, IL": "Arlington",
-    "Heights, NJ": "Hasbrouck",
-    "Hill, NJ": "Cherry",
-    "Island, NY": "Long",
-    "Jose, CA": "San",
-    "Juan, PR": "San",
-    "Lac, WI": "Fond du",
-    "Laoghaire, Ireland": "Dun",
-    "Lake, OH": "Indian",
-    "Lauderdale, FL": "Fort",
-    "Laurel, NJ": "Mt.",
-    "Louis, MO": "St.",
-    "Luzerne, NY": "Lake",
-    "Mateo, CA": "San",
-    "Moines, IA": "Des",
-    "Mountain, GA": "Pine",
-    "Oak, FL": "Live",
-    "Orleans, LA": "New",
-    "Park, AZ": "Litchfield",
-    "Park, MD": "Lexington",
-    "Park, MN": ["St. Louis", "Brooklyn"],
-    "Paso, TX": "El",
-    "Pass, WA": "Snoqualmie",
-    "Paul, MN": "St.",
-    "Petersburg, FL": "St.",
-    "Plainfield, NJ": "South",
-    "Plains, NY": "White",
-    "Point, NC": "High",
-    "Rock, AR": ["Little", "North Little"],
-    "Rosa, CA": "Santa",
-    "Sacromento, CA": "West",
-    "Sheen, UK": "East",
-    "Spring, MD": "Silver",
-    "Springs, CO": "Colorado",
-    "Springs, NY": "Saratoga",
-    "Station, TX": "College",
-    "Town, NY": "Rye",
-    "Vegas, NV": "Las",
-    "Vernon, WA": "Mount",
-    "Way, WA": "Federal",
-    "York, NY": "New"
-}
-
-# Look for a pattern of the form:
-#   in Word, XX
-#   where Word is one or more strings of letters each with an initial capital, the comma is optional, and XX is a pair of upper case letters
-# Note that this will also pick up roman-numeraled con names, E.g., Fantasycon XI, so we need to remove these later
-def ScanForLocales(s: str) -> Optional[Set[str]]:
-
-    # Find the first locale
-    # Detect locales of the form Name [Name..Name], XX  -- One or more capitalized words followed by an optional comma followed by exactly two UC characters
-    # ([A-Z][a-z]+\]*,?\s)+     Picks up one or more leading capitalized, space (or comma)-separated words
-    # \[*  and  \]*             Lets us ignore spans of [[brackets]]
-    # The "[^a-zA-Z]"           Prohibits another letter immediately following the putative 2-UC state
-    s1=s.replace("[", "").replace("]", "")   # Remove brackets
-    m=re.search("in ([A-Z][a-z]+\s+)?([A-Z][a-z]+\s+)?([A-Z][a-z]+,?\s+)([A-Z]{2})[^a-zA-Z]", " "+s1+" ")    # The extra spaces are so that there is at least one character before and after a possible locale
-    if m is not None and len(m.groups()) > 1:
-        groups=[x for x in m.groups() if x is not None]
-        city=" ".join(groups[0:-1])
-        city=city.replace(",", " ")                         # Get rid of commas
-        city=re.sub("\s+", " ", city).strip()               # Multiple spaces go to single space and trim the result
-        city=city.split()
-
-        state=groups[-1].strip()
-
-        impossiblestates = {"SF", "MC", "PR", "II", "IV", "VI", "IX", "XI", "XX", "VL", "XL", "LV", "LX"}  # PR: Progress Report; others Roman numerals; "LI" is allowed because of Long Island
-        if state not in impossiblestates:
-            # City should consist of one or more space-separated capitalized tokens. Split them into a list
-            if len(city) > 0:
-                skippers = {"Astra", "Con"}  # Second word of multi-word con names
-                if city[-1] not in skippers:
-                    # OK, now we know we have at least the form "in Xxxx[,] XX", but there may be many capitalized words before the Xxxx.
-                    # If not -- if we have *exactly* "in Xxxx[,] XX" -- then we have a local (as best we can tell).  Return it.
-                    loc = city[-1]+", "+state
-                    if len(city) == 1:
-                        return {loc}
-                    # Apparently we have more than one leading word.  Check the last word+state against the multiWordCities dictionary.
-                    # If the multi-word city is found, we're good.
-                    if loc in multiWordCities.keys():
-                        # Check the preceding token in the name against the token in multiWordCities
-                        tokens=multiWordCities[loc]
-                        if type(tokens) == str:
-                            if tokens == " ".join(city[:-1]):
-                                return {tokens+" "+loc}
-                        else:
-                            # It's a list of strings
-                            for token in tokens:
-                                if token == " ".join(city[:-1]):
-                                    return {token+" "+loc}
-
-
-    # OK, we can't find the Xxxx, XX pattern
-    # Look for 'in'+city+[,]+spelled-out country
-    # We'll look for a country name preceded by the word 'in' and one or two Capitalized words
-    countries=["Australia", "Belgium", "Bulgaria", "Canada", "China", "England", "Germany", "Holland", "Ireland", "Israel", "Italy", "New Zealand", "Netherlands", "Norway", "Sweden", "Finland", "Japan", "France",
-               "Poland", "Russia", "Scotland", "Wales"]
-    out: Set[str]=set()
-    s1=s.replace("[", "").replace("]", "")   # Remove brackets
-    splt = SplitOnSpan(",.\s", s1)  # Split on spans of comma, period, and space
-    for country in countries:
-        try:
-            loc=splt.index(country)
-            if loc > 2:     # Minimum is 'in City, Country'
-                locale=country
-                sep=", "
-                for i in range(1,6):    # City can be up to five tokens
-                    if loc-i < 0:
-                        break
-                    if re.match("^[A-Z]{1}[a-z]+$", splt[loc-i]):   # Look for Xxxxx
-                        locale=splt[loc-i]+sep+locale
-                    if splt[loc-i-1] == "in":
-                        return {locale}
-                    sep=" "
-        except ValueError as e:
-            continue
-
-    # Look for the pattern "in [[City Name]]"
-    # This has the fault that it can find something like "....in [[John Campbell]]'s report" and think that "John Campbell" is a locale.
-    # Fortunately, this will nearly always happen *after* the first sentence which contains the actual locale, and we ignore second and later hits
-        # Pattern:
-        # Capture "in" followed by "[[" followed by a group
-        # The group is a possibly repeated non-capturing group
-        #       which is a UC letter followed by one or more letters followed by an optional period or comma followed by zero or more spaces
-        # ending with "]]"
-    lst=re.findall("in \[\[((?:[A-Z][A-Za-z]+[.,]?\s*)+)]]", s)
-    if len(lst) > 0:
-        out.add(BaseFormOfLocaleName(localeBaseForms, lst[0]))
-    return out
-
+    Locale().AddF3Page(page, fancyPagesDictByWikiname)
 
 Log("***Analyzing convention series tables")
 
@@ -362,7 +161,7 @@ for page in fancyPagesDictByWikiname.values():
                     if locColumn is not None:
                         if locColumn < len(row) and len(row[locColumn]) > 0:
                             loc=WikiExtractLink(row[locColumn])
-                            conlocation=BaseFormOfLocaleName(localeBaseForms, loc)
+                            conlocation=Locale().BaseFormOfLocaleName(loc)
 
                     # Check the row for (virtual) in any form. If found, set the virtual flag and remove the text from the line
                     virtual=False
@@ -669,7 +468,7 @@ with open("Con location discrepancies.txt", "w+", encoding='utf-8') as f:
 
         # If it's an individual convention page and doesn't have a Locale, we search through its text for something that looks like a placename.
         #TODO: Shouldn't we move this upwards and store the derived location in otherwise-empty page.Locales?
-        m=ScanForLocales(page.Source)
+        m=Locale().ScanForLocales(page.Source)
         if len(m) > 0:
             for place in m:
                 place=WikiExtractLink(place)
@@ -686,7 +485,7 @@ with open("Con location discrepancies.txt", "w+", encoding='utf-8') as f:
 # Normalize convention locations to the standard City, ST form.
 Log("***Normalizing con locations")
 for con in conventions:
-    loc=ScanForLocales(con.Loc)
+    loc=Locale().ScanForLocales(con.Loc)
     if len(loc) > 1:
         Log("  In "+con.NameInSeriesList+"  found more than one location: "+str(loc))
     if len(loc) > 0:
@@ -920,8 +719,8 @@ Log("***Writing reports")
 # Write out a file containing canonical names, each with a list of pages which refer to it.
 # The format will be
 #     **<canonical name>
-#     <referring page>
-#     <referring page>
+#       <referring page>
+#       <referring page>
 #     ...
 #     **<canonical name>
 #     ...
---
+++ Locale.py
@@ -0,0 +1,219 @@
+from __future__ import annotations
+from typing import Dict, Set, Optional
+
+import re
+
+from F3Page import F3Page
+from Log import Log, LogSetHeader
+from HelpersPackage import SplitOnSpan
+
+class Locale:
+    locales: Set[str]=set()  # We use a set to eliminate duplicates and to speed checks
+    localeBaseForms: Dict[str, str]={}  # It's defined as a dictionary with the value being the base form of the key
+
+    def AddF3Page(self, page: F3Page, fancyPagesDictByWikiname: Dict[str, F3Page]) -> None:
+        # Add locale pages to the set
+        if page.IsLocale:
+            LogSetHeader("Found Locale page "+page.Name)
+            self.locales.add(page.Name)
+        else:
+            # If this page is a redirect to a locale page, add this page to the locale set
+            # TODO: Do we want all redirects to locale pages or just those tagged as a locale?
+            if page.Redirect != "" and page.Redirect in fancyPagesDictByWikiname.keys():
+                if fancyPagesDictByWikiname[page.Redirect].IsLocale:
+                    LogSetHeader("Processing Locale redirect "+page.Name)
+                    self.locales.add(page.Name)
+
+
+    # Convert names like "Chicago" to "Chicago, IL"
+    # We look through the locales database for names that are proper extensions of the input name
+    # First create the dictionary we'll need
+    def AddBaseForm(self, locale: str) -> None:
+        # Look for names of the form Name,ST
+        m=re.match("^([A-Za-z .]*),\s([A-Z]{2})$", locale)
+        if m is not None:
+            city=m.groups()[0]
+            state=m.groups()[1]
+            self.localeBaseForms.setdefault(city, city+", "+state)
+
+    # Find the base form of a locale.  E.g., the base form of "Cambridge, MA" is "Boston, MA".
+    def BaseFormOfLocaleName(self, name: str) -> str:
+        # Handle the (few) special cases where names may be confusing.
+        # There are certain names which are the names of minor cities and towns (usually written as "Name, XX") and also important cities which are written just "Name"
+        # E.g., "London, ON" and "London" or "Dublin, OH" and "Dublin"
+        # When the name appears without state (or whatever -- this is mostly a US & Canada problem) if it's in the list below, we assume it's a base form
+        # Note that we only add to this list when there is a *fannish* conflict.
+        basetable=["London", "Dublin"]
+        if name in basetable:
+           return name
+
+        # OK, try to find a base name
+        if name in self.localeBaseForms.keys():
+            return self.localeBaseForms[name]
+        return name
+
+    # The current algorithm messes up multi-word city names and only catches the last word.
+    # Correct the ones we know of to the full name.
+    multiWordCities={
+        "Angeles, CA": "Los",
+        "Antonio, TX": "San",
+        "Barbara, CA": "Santa",
+        "Beach, CA": ["Long", "Huntington"],
+        "Beach, FL": ["West Palm", "Cocoa", "Palm"],
+        "Beach, VA": "Virginia",
+        "Bend, IN": "South",
+        "Brook, IL": "Oak",
+        "Brook, LI": "Stony",
+        "Brook, NJ": "Saddle",
+        "Brook, NY": ["Stony", "Rye"],
+        "Brunswick, NJ": "New",
+        "Carrollton, MD": "New",
+        "Charles, IL": "St.",
+        "Christi, TX": "Corpus",
+        "City, IA": "Iowa",
+        "City, KY": "Park",
+        "City, MO": "Kansas",
+        "City, OK": "Oklahoma",
+        "City, UT": "Salt Lake",
+        "City, VA": "Crystal",
+        "Collins, CO": "Fort",
+        "Creek, CA": "Walnut",
+        "Creek, MI": "Battle",
+        "Diego, CA": "San",
+        "Elum, WA": "Cle",
+        "Falls, NY": "Niagara",
+        "Francisco, CA": "San",
+        "Grande, AZ": "Casa",
+        "Green, KY": "Bowling",
+        "Guardia, NY": "La",
+        "Harbor, NH": "Center",
+        "Heights, IL": "Arlington",
+        "Heights, NJ": "Hasbrouck",
+        "Hill, NJ": "Cherry",
+        "Island, NY": "Long",
+        "Jose, CA": "San",
+        "Juan, PR": "San",
+        "Lac, WI": "Fond du",
+        "Laoghaire, Ireland": "Dun",
+        "Lake, OH": "Indian",
+        "Lauderdale, FL": "Fort",
+        "Laurel, NJ": "Mt.",
+        "Louis, MO": "St.",
+        "Luzerne, NY": "Lake",
+        "Mateo, CA": "San",
+        "Moines, IA": "Des",
+        "Mountain, GA": "Pine",
+        "Oak, FL": "Live",
+        "Orleans, LA": "New",
+        "Park, AZ": "Litchfield",
+        "Park, MD": "Lexington",
+        "Park, MN": ["St. Louis", "Brooklyn"],
+        "Paso, TX": "El",
+        "Pass, WA": "Snoqualmie",
+        "Paul, MN": "St.",
+        "Petersburg, FL": "St.",
+        "Plainfield, NJ": "South",
+        "Plains, NY": "White",
+        "Point, NC": "High",
+        "Rock, AR": ["Little", "North Little"],
+        "Rosa, CA": "Santa",
+        "Sacromento, CA": "West",
+        "Sheen, UK": "East",
+        "Spring, MD": "Silver",
+        "Springs, CO": "Colorado",
+        "Springs, NY": "Saratoga",
+        "Station, TX": "College",
+        "Town, NY": "Rye",
+        "Vegas, NV": "Las",
+        "Vernon, WA": "Mount",
+        "Way, WA": "Federal",
+        "York, NY": "New"
+    }
+
+    # Look for a pattern of the form:
+    #   in Word, XX
+    #   where Word is one or more strings of letters each with an initial capital, the comma is optional, and XX is a pair of upper case letters
+    # Note that this will also pick up roman-numeraled con names, E.g., Fantasycon XI, so we need to remove these later
+    def ScanForLocales(self, s: str) -> Optional[Set[str]]:
+
+        # Find the first locale
+        # Detect locales of the form Name [Name..Name], XX  -- One or more capitalized words followed by an optional comma followed by exactly two UC characters
+        # ([A-Z][a-z]+\]*,?\s)+     Picks up one or more leading capitalized, space (or comma)-separated words
+        # \[*  and  \]*             Lets us ignore spans of [[brackets]]
+        # The "[^a-zA-Z]"           Prohibits another letter immediately following the putative 2-UC state
+        s1=s.replace("[", "").replace("]", "")  # Remove brackets
+        m=re.search("in ([A-Z][a-z]+\s+)?([A-Z][a-z]+\s+)?([A-Z][a-z]+,?\s+)([A-Z]{2})[^a-zA-Z]",
+                    " "+s1+" ")  # The extra spaces are so that there is at least one character before and after a possible locale
+        if m is not None and len(m.groups())>1:
+            groups=[x for x in m.groups() if x is not None]
+            city=" ".join(groups[0:-1])
+            city=city.replace(",", " ")  # Get rid of commas
+            city=re.sub("\s+", " ", city).strip()  # Multiple spaces go to single space and trim the result
+            city=city.split()
+
+            state=groups[-1].strip()
+
+            impossiblestates={"SF", "MC", "PR", "II", "IV", "VI", "IX", "XI", "XX", "VL", "XL", "LV",
+                              "LX"}  # PR: Progress Report; others Roman numerals; "LI" is allowed because of Long Island
+            if state not in impossiblestates:
+                # City should consist of one or more space-separated capitalized tokens. Split them into a list
+                if len(city)>0:
+                    skippers={"Astra", "Con"}  # Second word of multi-word con names
+                    if city[-1] not in skippers:
+                        # OK, now we know we have at least the form "in Xxxx[,] XX", but there may be many capitalized words before the Xxxx.
+                        # If not -- if we have *exactly* "in Xxxx[,] XX" -- then we have a local (as best we can tell).  Return it.
+                        loc=city[-1]+", "+state
+                        if len(city) == 1:
+                            return {loc}
+                        # Apparently we have more than one leading word.  Check the last word+state against the multiWordCities dictionary.
+                        # If the multi-word city is found, we're good.
+                        if loc in self.multiWordCities.keys():
+                            # Check the preceding token in the name against the token in multiWordCities
+                            tokens=self.multiWordCities[loc]
+                            if type(tokens) == str:
+                                if tokens == " ".join(city[:-1]):
+                                    return {tokens+" "+loc}
+                            else:
+                                # It's a list of strings
+                                for token in tokens:
+                                    if token == " ".join(city[:-1]):
+                                        return {token+" "+loc}
+
+        # OK, we can't find the Xxxx, XX pattern
+        # Look for 'in'+city+[,]+spelled-out country
+        # We'll look for a country name preceded by the word 'in' and one or two Capitalized words
+        countries=["Australia", "Belgium", "Bulgaria", "Canada", "China", "England", "Germany", "Holland", "Ireland",
+                   "Israel", "Italy", "New Zealand", "Netherlands", "Norway", "Sweden", "Finland", "Japan", "France",
+                   "Poland", "Russia", "Scotland", "Wales"]
+        out: Set[str]=set()
+        s1=s.replace("[", "").replace("]", "")  # Remove brackets
+        splt=SplitOnSpan(",.\s", s1)  # Split on spans of comma, period, and space
+        for country in countries:
+            try:
+                loc=splt.index(country)
+                if loc>2:  # Minimum is 'in City, Country'
+                    locale=country
+                    sep=", "
+                    for i in range(1, 6):  # City can be up to five tokens
+                        if loc-i<0:
+                            break
+                        if re.match("^[A-Z]{1}[a-z]+$", splt[loc-i]):  # Look for Xxxxx
+                            locale=splt[loc-i]+sep+locale
+                        if splt[loc-i-1] == "in":
+                            return {locale}
+                        sep=" "
+            except ValueError as e:
+                continue
+
+        # Look for the pattern "in [[City Name]]"
+        # This has the fault that it can find something like "....in [[John Campbell]]'s report" and think that "John Campbell" is a locale.
+        # Fortunately, this will nearly always happen *after* the first sentence which contains the actual locale, and we ignore second and later hits
+        # Pattern:
+        # Capture "in" followed by "[[" followed by a group
+        # The group is a possibly repeated non-capturing group
+        #       which is a UC letter followed by one or more letters followed by an optional period or comma followed by zero or more spaces
+        # ending with "]]"
+        lst=re.findall("in \[\[((?:[A-Z][A-Za-z]+[.,]?\s*)+)]]", s)
+        if len(lst)>0:
+            out.add(Locale().BaseFormOfLocaleName(lst[0]))
+        return out
